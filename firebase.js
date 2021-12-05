1f903e01126ef24c74d4ad1f3b25715d81d10ea2
jojoma8 <jojo_ma@hotmail.co.uk>
2021-12-05 21:07:26 +0000

    allEnv

--- firebase.js
+++ firebase.js
@@ -15,13 +15,12 @@ import { useState, useEffect } from "react";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-  // authDomain: "nextjswithfirebaseauth.firebaseapp.com",
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: "nextjswithfirebaseauth",
-  storageBucket: "nextjswithfirebaseauth.appspot.com",
-  messagingSenderId: "904610825122",
-  appId: "1:904610825122:web:7d56445df312b909a9a8fb",
-  measurementId: "G-4KLN20MK4R",
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
 };
 
 // Initialize Firebase
